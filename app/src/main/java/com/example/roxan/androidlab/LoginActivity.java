66ad917cd85d93fee25f33463ccdedae5a6e565a
roxan <shap0015@algonquinlive.com>
2018-01-24 11:59:13 -0500

    Initial commit

--- app/src/main/java/com/example/roxan/androidlab/LoginActivity.java
+++ app/src/main/java/com/example/roxan/androidlab/LoginActivity.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class LoginActivity extends Activity {
-
+//function onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
     }
 }
